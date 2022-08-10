83205c1f33d822f088ca77c9909e2024b46f371b
hgjazhgj <hgjazhgj[at]gmail.com>
2022-08-10 16:13:27 +0800

    v0.0.1

---
+++ FGO-ExpBall/fgoExpBall.py
@@ -0,0 +1,120 @@
+import argparse,cmd,functools,json,os,platform,re,signal,time
+import fgoDevice
+from fgoConst import VERSION
+import fgoLogging
+from fgoFuse import fuse
+import fgoKernel
+from fgoSchedule import ScriptStop,schedule
+logger=fgoLogging.getLogger('Cli')
+
+def wrapTry(func):
+    @functools.wraps(func)
+    def wrapper(self,*args,**kwargs):
+        try:return func(self,*args,**kwargs)
+        except ArgError as e:
+            if e.args[0]is not None:logger.error(e)
+        except KeyboardInterrupt:logger.critical('KeyboardInterrupt')
+        except BaseException as e:logger.exception(e)
+        finally:self.prompt=f'FGO-ExpBall@{fgoDevice.device.name}> '
+    return wrapper
+def countdown(x):
+    timer=time.time()+x
+    while(rest:=timer-time.time())>0:
+        print((lambda sec:f'{sec//3600:02}:{sec%3600//60:02}:{sec%60:02}')(round(rest)),end=' \r')
+        time.sleep(min(1,max(0,rest)))
+
+class Cmd(cmd.Cmd,metaclass=lambda name,bases,attrs:type(name,bases,{i:wrapTry(j)if i.startswith('do_')else j for i,j in attrs.items()})):
+    intro=f'''
+FGO-ExpBall {VERSION}, Copyright (c) 2019-2022 hgjazhgj
+
+Connect device first, then type main to make ExpBall.
+Type help or ? to list commands, help <command> to get more information.
+Some commands support <command> [<subcommand> ...] {{-h, --help}} for further information.
+'''
+    prompt='FGO-py@Device> '
+    def __init__(self):
+        super().__init__()
+        fgoDevice.Device.enumDevices()
+    def emptyline(self):return
+    def precmd(self,line):
+        if line:logger.info(line)
+        return line
+    def completenames(self,text,*ignored):return[f'{i} 'for i in super().completenames(text,*ignored)]
+    def completecommands(self,table,text,line,begidx,endidx):return sum([[f'{k} 'for k in j if k.startswith(text)]for i,j in table.items()if re.match(f'{i}$',' '.join(line.split()[1:None if begidx==endidx else -1]))],[])
+    def do_exec(self,line):exec(line)
+    def do_shell(self,line):os.system(line)
+    def do_exit(self,line):
+        'Exit FGO-py'
+        return True
+    def do_EOF(self,line):return self.do_exit(line)
+    def do_version(self,line):
+        'Show FGO-py version'
+        print(VERSION)
+    def do_connect(self,line):
+        'Connect to a device'
+        arg=parser_connect.parse_args(line.split())
+        if arg.list:return print(*fgoDevice.Device.enumDevices(),sep='\n')
+        fgoDevice.device=fgoDevice.Device(arg.name)
+    def complete_connect(self,text,line,begidx,endidx):
+        return self.completecommands({
+            '':['wsa','win']+[f'/{i}'for i in fgoDevice.helpers]+fgoDevice.Device.enumDevices()
+        },text,line,begidx,endidx)
+    def do_main(self,line):
+        'Loop for battle until AP empty'
+        arg=parser_main.parse_args(line.split())
+        self.work=fgoKernel.ExpBall()
+        self.do_continue(f'-s {arg.sleep}')
+    def do_continue(self,line):
+        'Continue last battle after abnormal break'
+        arg=parser_main.parse_args(line.split())
+        assert fgoDevice.device.available
+        countdown(arg.sleep)
+        try:
+            signal.signal(signal.SIGINT,lambda*_:schedule.stop())
+            if platform.system()=='Windows':signal.signal(signal.SIGBREAK,lambda*_:schedule.pause())
+            self.work()
+        except ScriptStop as e:
+            logger.critical(e)
+        except KeyboardInterrupt:
+            raise
+        except BaseException as e:
+            logger.exception(e)
+        finally:
+            signal.signal(signal.SIGINT,signal.SIG_DFL)
+            if platform.system()=='Windows':signal.signal(signal.SIGBREAK,signal.SIG_DFL)
+            fuse.reset()
+            schedule.reset()
+    def do_169(self,line):
+        'Adapt none 16:9 screen'
+        arg=parser_169.parse_args(line.split())
+        assert fgoDevice.device.available
+        getattr(fgoDevice.device,f'{arg.action}169')()
+    def complete_169(self,text,line,begidx,endidx):
+        return self.completecommands({
+            '':['invoke','revoke']
+        },text,line,begidx,endidx)
+
+ArgError=type('ArgError',(Exception,),{})
+def validator(type,func,desc='\b'):
+    def f(x):
+        if not func(x:=type(x)):raise ValueError
+        return x
+    f.__name__=desc
+    return f
+class ArgParser(argparse.ArgumentParser):
+    def exit(self,status=0,message=None):raise ArgError(message)
+
+parser_main=ArgParser(prog='main',description=Cmd.do_main.__doc__)
+parser_main.add_argument('-s','--sleep',help='Sleep before run (default: %(default)s)',type=validator(float,lambda x:x>=0,'nonnegative'),default=0)
+
+parser_connect=ArgParser(prog='connect',description=Cmd.do_connect.__doc__)
+parser_connect.add_argument('-l','--list',help='List all available devices',action='store_true')
+parser_connect.add_argument('name',help='Device name (default to the last connected one)',default='',nargs='?')
+
+parser_169=ArgParser(prog='169',description=Cmd.do_169.__doc__)
+parser_169.add_argument('action',help='Action',type=str.lower,choices=['invoke','revoke'])
+
+def main(args):Cmd().cmdloop()
+
+if __name__=='__main__':
+    main([])
---
+++ FGO-ExpBall/fgoKernel.py
@@ -0,0 +1,142 @@
+from fgoConst import *
+from fgoDetect import *
+from fgoLogging import getLogger
+from fgoSchedule import schedule,ScriptStop
+logger=getLogger('Kernel')
+
+class ExpBall:
+    def __init__(self):
+        self.runOnce=True
+
+    def __call__(self):
+        while True:
+            # 抽友情
+            SPACE.click(.5)
+            if MAIN_SUMMON.appear():
+                MAIN_SUMMON.click(5)
+                BACK.wait(1)
+            else:
+                SPACE.click(.8)
+            while not SUMMON_FP.appear():
+                SUMMON_SWITCH.click(2.5)
+            SUMMON_SUMMON.click(.8)
+            while not SUMMON_SALE.appear():
+                SUMMON_SUBMIT.click(3)
+                while not SUMMON_CONTINUE.appear():
+                    SPACE.click(.4)
+                if t:=Detect().findSpecial():
+                    if Detect.cache.countSpecial()>1:
+                        raise ScriptStop('Lot of Special Summoned')
+                    Button(t).click(2)
+                    Button((32,180)).click(1)
+                    BACK.click(1.5)
+                SUMMON_CONTINUE.click(.8)
+            # 卖从者
+            SUMMON_SALE.click(4)
+            SELECT_FINISH.wait(1)
+            SELECT_SERVANT.click(2)
+            if self.runOnce: # 每行7个 获得顺序 智能筛选
+                while not SELECT_GIRD.appear():
+                    SELECT_GIRD.click(2)
+                SORT_SORT.click(.8)
+                SORT_BYTIME.click(.5)
+                while not SORT_FILTER_ON.appear():
+                    SORT_FILTER_ON.click(.8)
+                SORT_SUBMIT.click(.8)
+            self.sell()
+            # 卖礼装
+            SELECT_REISOU.click(2)
+            if self.runOnce: # 每行7个 3星 智能筛选
+                while not SELECT_GIRD.appear():
+                    SELECT_GIRD.click(2)
+                FILTER_FILTER.click(.8)
+                FILTER_RESET.click(.5)
+                FILTER_STAR_3.click(.5)
+                FILTER_SUBMIT.click(.8)
+                SORT_SORT.click(.8)
+                while not SORT_FILTER_ON.appear():
+                    SORT_FILTER_ON.click(.8)
+                SORT_SUBMIT.click(.8)
+            self.sell()
+            # 卖纹章
+            SELECT_CODE.click(2)
+            if self.runOnce: # 每行7个 12星
+                while not SELECT_GIRD.appear():
+                    SELECT_GIRD.click(2)
+                FILTER_FILTER.click(.8)
+                FILTER_RESET.click(.5)
+                FILTER_STAR_1.click(.5)
+                FILTER_STAR_2.click(.5)
+                FILTER_SUBMIT.click(.8)
+            self.sell()
+            # 礼装强化
+            BACK.click(3)
+            SPACE.click(.5)
+            MAIN_SYNTHESIS.click(4)
+            SYNTHESIS_BEGIN.click(3)
+            SYNTHESIS_LOAD.wait().click(3)
+            if self.runOnce: # 每行7个 1星 等级顺序 智能筛选 降序
+                while not SELECT_GIRD.appear():
+                    SELECT_GIRD.click(2)
+                FILTER_FILTER.click(.8)
+                FILTER_RESET.click(.5)
+                FILTER_STAR_1.click(.5)
+                FILTER_SUBMIT.click(.8)
+                SORT_SORT.click(.8)
+                SORT_BYLEVEL.click(.5)
+                while not SORT_FILTER_ON.appear():
+                    SORT_FILTER_ON.click(.8)
+                SORT_SUBMIT.click(.8)
+                while SORT_INC.appear():
+                    SORT_INC.click(.8)
+            if not SELECT_LOCK.appear():
+                SYNTHESIS_LOCK.click(1)
+                SELECT_LOCK.offset((60,0)).click(1)
+                SYNTHESIS_SELECT.click(1)
+            SELECT_LOCK.offset((60,0)).click(1.5)
+            BACK.wait(.5)
+            SYNTHESIS_ENTER.click(1)
+            SELECT_FINISH.wait(1)
+            if self.runOnce: # 每行7个 12星 稀有度顺序 智能筛选 降序
+                while not SELECT_GIRD.appear():
+                    SELECT_GIRD.click(2)
+                FILTER_FILTER.click(.8)
+                FILTER_RESET.click(.5)
+                FILTER_STAR_1.click(.5)
+                FILTER_STAR_2.click(.5)
+                FILTER_SUBMIT.click(.8)
+                SORT_SORT.click(.8)
+                SORT_BYRANK.click(.5)
+                while not SORT_FILTER_ON.appear():
+                    SORT_FILTER_ON.click(.8)
+                SORT_SUBMIT.click(.8)
+                while SORT_INC.appear():
+                    SORT_INC.click(.8)
+            while True:
+                self.selectAll()
+                if SELECT_FINISH.appear():
+                    break
+                SELECT_FINISH.click(1)
+                SELECT_FINISH.click(.5)
+                SUMMON_SUBMIT.click(2)
+                while not BACK.appear():
+                    SPACE.click(.2)
+                SYNTHESIS_ENTER.click(1)
+                SELECT_FINISH.wait(1)
+            BACK.click(1).click(2).wait()
+            self.runOnce=False
+
+    def selectAll(self):
+        for i in range(4):
+            for j in range(7):
+                Button((133+133*j,253+140*i)).click(.15)
+
+    def sell(self):
+        while True:
+            self.selectAll()
+            if SELECT_FINISH.appear():
+                break
+            SELECT_FINISH.click(1)
+            SORT_SUBMIT.click(1)
+            SELL_RESULT.click(1)
+            SELECT_FINISH.wait(1)
